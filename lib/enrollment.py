94863227e7403414e7fb365600ecbf69dbad00de
Joeken10 <joekentafir@gmail.com>
2024-09-07 17:55:45 +0300

    first commit

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,23 +1,35 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}  # Dictionary to store grades per enrollment
 
-    def enroll(self, course):
+    def enroll(self, course, grade=None):
         if isinstance(course, Course):
             enrollment = Enrollment(self, course)
             self._enrollments.append(enrollment)
             course.add_enrollment(enrollment)
+            if grade is not None:
+                self._grades[enrollment] = grade
         else:
             raise TypeError("course must be an instance of Course")
 
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def aggregate_average_grade(self):
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        return total_grades / num_courses if num_courses > 0 else 0
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -45,3 +57,4 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    
